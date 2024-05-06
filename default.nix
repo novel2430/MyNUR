c82b9f37d4bd8103eec31c8cd5cdabb40087773b
novel2430 <novel2430@163.com>
2024-05-07 00:14:14 +0800

    delete brave

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ unstable, pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { } }:
 
 {
   # The `lib`, `modules`, and `overlays` names are special
