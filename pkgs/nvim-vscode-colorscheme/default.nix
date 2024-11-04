277a252f6b2c5418a08af45d5cd41a779384d4c1
novel2430 <novel2430@163.com>
2024-11-04 15:20:14 +0800

    add neovim vscode colorscheme

--- pkgs/nvim-vscode-colorscheme/default.nix
+++ pkgs/nvim-vscode-colorscheme/default.nix
@@ -4,8 +4,8 @@ with pkgs;
 vimUtils.buildVimPlugin {
   name = "vscode-nvim";
   src = builtins.fetchGit {
-    url = "https://github.com/novel2430/vscode.nvim.git";
+    url = "https://github.com/Mofiqul/vscode.nvim.git";
     ref = "main";
-    rev = "1b4d5dc51539f0ade4bb1ffc1fbae41b5f8846ec";
+    rev = "7de58b7a6d55fe48475d0ba2fddbcec871717761";
   };
 }
